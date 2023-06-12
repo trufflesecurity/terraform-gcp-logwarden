cbf9ba42ed0673e42d0a37346759d5a4c9bc090a
Mike Vanbuskirk <mike.vanbuskirk@trufflesec.com>
2023-06-12 19:33:42 -0400

    remove liveness probe, change startup to tcp

--- main.tf
+++ main.tf
@@ -24,16 +24,10 @@ resource "google_cloud_run_v2_service" "main" {
       ports {
         container_port = 8080
       }
-      liveness_probe {
-        initial_delay_seconds = 240
-        timeout_seconds       = 2
-        http_get {
-        }
-      }
 
       startup_probe {
         initial_delay_seconds = 120
-        http_get {
+        tcp_socket {
         }
       }
     }
